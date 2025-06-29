ff69cc5ac9abdf8064358e07b1fb78489f974f66
Gabriel Barros <gabriellourencobarros02@gmail.com>
2025-06-29 16:17:12 -0300

    adicionando melhorias

--- CTRLInvesting.Api/Services/TaxasAnuaisService.cs
+++ CTRLInvesting.Api/Services/TaxasAnuaisService.cs
@@ -1,5 +1,7 @@
 using CTRLInvesting.Api.Interfaces;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Globalization;
 
 namespace CTRLInvesting.Api.Services;
 
@@ -14,11 +16,25 @@ public class TaxasAnuaisService : ITaxasAnuaisService
 
     public async Task<double> GetSelic()
     {
-        string url = "https://flask-production-90940.up.railway.app/gab2020/taxa/432";
-        var response = await _httpClient.GetAsync(url);
-        var jsonString = await response.Content.ReadAsStringAsync();
-        var stockData = JsonConvert.DeserializeObject<double>(jsonString);
-        return stockData/100;
+        double stockData = await GetTaxaSelic();
+        return stockData / 100;
+    }
+
+    async Task<double> GetTaxaSelic()
+    {
+        // URL para o último valor da SELIC diária
+        string url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.432/dados/ultimos/1?formato=json";
+
+        using var client = new HttpClient();
+        var response = await client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+
+        var json = await response.Content.ReadAsStringAsync();
+        var tax = JsonConvert.DeserializeObject<List<BcbSgsResponse>>(json)[0];
+
+        if (double.TryParse(tax.valor, new CultureInfo("en-US"), out double value))
+            return value;
+        return 0;
     }
 
     public async Task<double> GetCdi()
@@ -27,6 +43,12 @@ public class TaxasAnuaisService : ITaxasAnuaisService
         var response = await _httpClient.GetAsync(url);
         var jsonString = await response.Content.ReadAsStringAsync();
         var stockData = JsonConvert.DeserializeObject<double>(jsonString);
-        return stockData/100;
+        return stockData / 100;
+    }
+
+    class BcbSgsResponse
+    {
+        public string data { get; set; }
+        public string valor { get; set; }
     }
 }
